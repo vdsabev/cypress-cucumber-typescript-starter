75d4ee4611163de9fbb6b19c5ad3361579a4b278
Vladimir Sabev <vdsabev@gmail.com>
2020-12-05 18:46:51 +0200

    Integrate page static methods; fix error handling

--- cypress/integration/common/Page.ts
+++ cypress/integration/common/Page.ts
@@ -1,12 +1,40 @@
 import browser from './browser';
-import { registerPage } from './pages';
 
 export default abstract class Page {
+  // Static
+  private static pages: Page[] = [];
+
+  private static register(page: Page) {
+    const duplicatePage = this.pages.find(
+      (registeredPage) => registeredPage.constructor === page.constructor
+    );
+
+    if (duplicatePage != null) {
+      // NOTE: We log the dupplicate page's name because the new page doesn't have a name assigned yet
+      throw new Error(
+        `A page with name "${duplicatePage.name}" has already been registered`
+      );
+    }
+
+    this.pages.push(page);
+  }
+
+  public static find(name: string) {
+    const page = this.pages.find((page) => page.name === name);
+
+    if (!page) {
+      throw new Error(`Couldn't find a page with name "${name}"`);
+    }
+
+    return page;
+  }
+
+  // Instance
   public abstract readonly name: string;
   protected abstract readonly url: string;
 
   constructor() {
-    registerPage(this);
+    Page.register(this);
   }
 
   public open() {
--- cypress/integration/common/steps.ts
+++ cypress/integration/common/steps.ts
@@ -1,12 +1,12 @@
 import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
 
 import browser from './browser';
-import { findPage } from './pages';
+import Page from './Page';
 
 export * from 'cypress-cucumber-preprocessor/steps';
 
 Given(`(the customer) is on the {string} page`, (name: string) => {
-  findPage(name).open();
+  Page.find(name).open();
 });
 
 Then(`the page title should be {string}`, (title: string) => {
